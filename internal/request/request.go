0e018c21927b42fac78db073c34bdb2da516379f
aswad <aswad.m.tariq13@gmail.com>
2026-04-02 21:12:34 +0800

    chp5: headers dn

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,13 +4,16 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+
+	"aswad.http.module/internal/headers"
 )
 
 type parserState string
 
 const (
-	StateInit parserState = "init"
-	StateDone parserState = "done"
+	StateInit    parserState = "init"
+	StateHeaders parserState = "headers"
+	StateDone    parserState = "done"
 )
 
 type RequestLine struct {
@@ -21,12 +24,14 @@ type RequestLine struct {
 
 type Request struct {
 	RequestLine RequestLine
+	Headers     headers.Headers
 	state       parserState
 }
 
 func newRequest() *Request {
 	return &Request{
-		state: StateInit,
+		state:   StateInit,
+		Headers: *headers.NewHeaders(),
 	}
 }
 
@@ -66,9 +71,10 @@ func (r *Request) parse(data []byte) (int, error) {
 	read := 0
 outer:
 	for {
+		currentData := data[read:]
 		switch r.state {
 		case StateInit:
-			rl, n, err := parseRequestLine((data[read:]))
+			rl, n, err := parseRequestLine((currentData))
 			if err != nil {
 				return 0, err
 			}
@@ -80,10 +86,26 @@ outer:
 			r.RequestLine = *rl
 			read += n
 
-			r.state = StateDone
+			r.state = StateHeaders
+		case StateHeaders:
+			n, done, err := r.Headers.Parse(currentData)
+			if err != nil {
+				return 0, err
+			}
 
+			if n == 0 {
+				break outer
+			}
+
+			read += n
+
+			if done {
+				r.state = StateDone
+			}
 		case StateDone:
 			break outer
+		default:
+			panic("somehow we have programmed poorly rip")
 		}
 	}
 	return read, nil
